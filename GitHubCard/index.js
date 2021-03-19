85b8a710e373e6d2af1949cb10ace44188911f21
JuanCalixto <juancalixtomorales@gmail.com>
2021-03-18 21:05:04 -0500

    iterate over users data creating cards for users and adding it to the DOM

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -41,6 +41,16 @@ axios.get('https://api.github.com/users/cal1x')
 
 const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
+followersArray.forEach(user => {
+  axios.get(`https://api.github.com/users/${user}`)
+  .then(res => {
+    cards.appendChild(createCard(res.data));
+  })
+  .catch(error => {
+    console.log('Error')
+  })
+})
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
